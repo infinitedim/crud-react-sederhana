f812d7db3553206d44b6c66bc752b5df61203295
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-31 08:43:30 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,38 @@ export const url = "https://musik98.herokuapp.com";
 
 export default function App() {
   const [musics, setMusics] = useState([]);
-  const [id, setId] = useState([]);
+  const [id, setId] = useState(""); // hanya untuk keyword pencarian
+  const [fetchedData, setData] = useState([]); // menerima data hasil pencarian
 
-  const formdata = new FormData(this);
-  formdata.append("_id", id);
+  // const formdata = new FormData(this);
+  // formdata.append("_id", id);
 
-  useEffect(() => {
-    fetch(`${url}/users/data`, {
+  async function getId(id) {
+    await fetch(`${url}/users/data/${id}`, {
       method: "GET",
     })
       .then((res) => res.json())
       .then((res) => {
-        setMusics(res.data);
-      })
-      .catch((e) => e);
-  }, [setMusics]);
-
-  function getId(id) {
-    fetch(`${url}/users/data/${id}`, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setId(res.data);
-        console.log(res.data);
+        setData(res.data); // setId(res.data) - jgn dicampur aduk sma keyword utk mencari data musiknya ya, mending buat state baru untuk memasukkan data hasil fetch ini
+        // console.log(res.data);
       })
       .catch((e) => e);
   }
+
+  useEffect(() => {
+    async function fetchMusic() {
+      await fetch(`${url}/users/data`, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setMusics(res.data);
+        })
+        .catch((e) => e);
+    }
+
+    fetchMusic();
+  }, [setMusics]);
 
   return (
     <main className="App">
@@ -49,22 +54,46 @@ export default function App() {
           <input
             type="text"
             className="search"
-            onChange={(e) => setId(e.target.value)}
+            value={id}
+            onChange={(e) => {
+              // note: setId klo emng hanya untuk menerima value dari input search,
+              //       mending jgn dicampur aduk sma data hasil fetch ya,
+              //       kurang direkomendasiin, soalnya tipe datanya beda(string & array);
+              setId(e.target.value);
+            }}
           />
           <input type="submit" value="Cari" />
         </form>
       </nav>
       <Form />
+
       <div className="card-container">
-        {musics.map((music) => (
-          <Card
-            key={music._id}
-            _id={music._id}
-            judul={music.judul}
-            penyanyi={music.penyanyi}
-            musik={`${url}/${music.musik}`}
-          />
-        ))}
+        {/* Conditional rendering */}
+        {fetchedData.length > 0 ? (
+          <>
+            {fetchedData.map((music) => (
+              <Card
+                key={music._id}
+                _id={music._id}
+                judul={music.judul}
+                penyanyi={music.penyanyi}
+                musik={`${url}/${music.musik}`}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {musics.map((music) => (
+              <Card
+                key={music._id}
+                _id={music._id}
+                judul={music.judul}
+                penyanyi={music.penyanyi}
+                musik={`${url}/${music.musik}`}
+              />
+            ))}
+          </>
+        )}
       </div>
     </main>
   );
